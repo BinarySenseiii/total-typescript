687708a414546f4f5c388195160650de5eb158ad
Faisal Tariq <faisaltariq183@gmail.com>
2024-02-23 16:14:45 -0800

    done: 01-inference-basics-challenges

--- type-transformations-workshop/src/01-inference-basics/04-get-object-keys.problem.ts
+++ type-transformations-workshop/src/01-inference-basics/04-get-object-keys.problem.ts
@@ -1,17 +1,19 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils'
 
 const testingFrameworks = {
-  vitest: {
-    label: "Vitest",
-  },
-  jest: {
-    label: "Jest",
-  },
-  mocha: {
-    label: "Mocha",
-  },
-};
+	vitest: {
+		label: 'Vitest',
+	},
+	jest: {
+		label: 'Jest',
+	},
+	mocha: {
+		label: 'Mocha',
+	},
+}
 
-type TestingFramework = unknown;
+// keyof will not work on runtime it will work only on types
 
-type tests = [Expect<Equal<TestingFramework, "vitest" | "jest" | "mocha">>];
+type TestingFramework = keyof typeof testingFrameworks
+
+type tests = [Expect<Equal<TestingFramework, 'vitest' | 'jest' | 'mocha'>>]
